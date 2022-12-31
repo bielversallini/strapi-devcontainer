df58def33dd9d52125aee47ce70708243dcef3a7
Gabriel Barbosa <gabriel.barbosa@faci.ly>
2022-12-31 12:55:28 -0300

    Update configs

--- config/database.js
+++ config/database.js
@@ -2,10 +2,15 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi-db"),
+      user: env("DATABASE_USERNAME", "strapi-user"),
+      password: env("DATABASE_PASSWORD", "strapi-pass"),
     },
+    debug: env.bool('DATABASE_DEBUG', false),
     useNullAsDefault: true,
   },
 });
